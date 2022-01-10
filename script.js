f75c88f2ccaa7074cf9e5bb5a0f7a81a3b5afae1
MarcoLotti <gorlandelli64@gmail.com>
2022-01-10 10:30:15 +0100

    indovina numeri corretto

---
+++ script.js
@@ -0,0 +1,49 @@
+const numeriTotali = 5;
+const numeriRandom = [];
+
+while(numeriRandom.length < numeriTotali){
+    const nuovoNumero = getrandomnumber(1,99);
+    if (!numeriRandom.includes(nuovoNumero)) {
+        numeriRandom.push(nuovoNumero);
+    }
+
+}
+document.getElementById('visualizza').innerHTML = numeriRandom;
+
+setTimeout(chediUtente, 30000);
+
+setTimeout(cancellaNumeri, 30500);
+
+function cancellaNumeri() {
+    document.getElementById('visualizza').innerHTML = "";
+}
+
+function chediUtente() {
+
+    const numeriUtente = [];
+
+    while(numeriUtente.length < numeriTotali){
+        const nuovoNumero = parseInt(prompt("inserisci il numero"));
+        if(!numeriUtente.includes(nuovoNumero)){
+            numeriUtente.push(nuovoNumero);
+        }  
+    }
+
+}
+
+function verificaNumeri(arrayUtente, arrayNumeri) {
+    const numeriIndovinati = [];
+
+    for (let i = 0; i < arrayUtente.length; i++) {
+        if (arrayNumeri.includes(arrayUtente[i])) {
+            numeriIndovinati.push(arrayUtente[i]);
+        }
+    }
+    
+    return numeriIndovinati;
+}
+
+function getrandomnumber(min, max) {
+    return Math.floor(math.random() * (max - min + 1) + min);
+}
+
